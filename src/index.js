a8d64fbbac9fb3ce61541ba84c682d2eb286386c
ZhangZexiao <zhangzexiaozhang@163.com>
2018-06-09 16:07:56 +0800

    Add react-snapshot for static webpage to host on zhangzexiao.github.io

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import { render } from 'react-snapshot';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Dashboard from './components/Dashboard';
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
+//render(<Dashboard />, document.getElementById('root'));
 registerServiceWorker();
